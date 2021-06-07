ca837cb169a5ac9c7c0b22a5fdb65be9b3cbff1e
Charles <charlesmelki@gmail.com>
2021-06-07 08:54:58 +0200

    Add HTMLHint

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "ace-builds/src-noconflict/mode-html"
 import "ace-builds/src-noconflict/theme-xcode"
 import Validator from "./Validator";
 import {useState} from "react";
+import {HTMLHint} from "htmlhint";
 
 function App() {
   const [code, setCode] = useState('')
+  const [HTMLHintErrors, setHTMLHintErrors] = useState(HTMLHint.verify(code, HTMLHint.rules))
 
   function onChange(newValue) {
     setCode(newValue)
+    setHTMLHintErrors(HTMLHint.verify(newValue, HTMLHint.rules))
   }
 
   return (
@@ -20,10 +23,10 @@ function App() {
         debounceChangePeriod={300}
         onChange={onChange}
         name={"editor"}
-        defaultValue={"<!DOCTYPE html><body></body>"}
+        defaultValue={code}
       />
       <div className={"validators"}>
-        <Validator name={"HTMLHint"}/>
+        <Validator name={"HTMLHint"} code={code} key={code} errors={HTMLHintErrors}/>
         <Validator name={"W3C (Nu)"}/>
         <Validator name={"HTML-validate"}/>
         <Validator name={"htmllint"}/>
--- src/Validator.js
+++ src/Validator.js
@@ -3,9 +3,26 @@ import "./Validator.css"
 function Validator(props) {
   return (
     <div className={"validator"}>
-      <h3>{props.name}</h3>
+      <h3>{props.name} [{props.errors ? props.errors.length : 0} errors]</h3>
       <div className={"errors"}>
-
+        {
+          props.errors ?
+            props.errors.map(error => {
+              return <div className={"error_div"}>
+                {
+                  Object.entries(error).map(key => {
+                    if (typeof key[1] === "object") {
+                      return <div><b>{`${key[0]}:`}</b> {` ${JSON.stringify(key[1])}`}</div>
+                    } else {
+                      return <div><b>{`${key[0]}:`}</b> {` ${key[1]}`}</div>
+                    }
+                  })
+                }
+              </div>
+            })
+            :
+            ''
+        }
       </div>
     </div>
   )
